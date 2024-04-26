b730bb5ea959345d487461be3c9739ab74cbb9f9
Garrett <59628188+littleprogrammerG@users.noreply.github.com>
2024-04-26 16:02:17 -0600

    Updated image location to pack in jar.

--- src/main/java/dev/littleprogrammer/Main.java
+++ src/main/java/dev/littleprogrammer/Main.java
@@ -2,6 +2,7 @@ package dev.littleprogrammer;
 
 import javax.swing.*;
 import java.awt.*;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,12 +20,12 @@ public class Main {
         frame.pack(); // Pack after adding components makes preferred size
 
         // Set icon
-        List<Image> icons = new ArrayList<Image>();
-        icons.add(new ImageIcon("src/Images/snakeIcon16.png").getImage());
-        icons.add(new ImageIcon("src/Images/snakeIcon24.png").getImage());
-        icons.add(new ImageIcon("src/Images/snakeIcon32.png").getImage());
-        icons.add(new ImageIcon("src/Images/snakeIcon48.png").getImage());
-        icons.add(new ImageIcon("src/Images/snakeIcon256.png").getImage());
+        List<Image> icons = new ArrayList<>();
+        icons.add(loadIcon("/snakeIcon16.png"));
+        icons.add(loadIcon("/snakeIcon24.png"));
+        icons.add(loadIcon("/snakeIcon32.png"));
+        icons.add(loadIcon("/snakeIcon48.png"));
+        icons.add(loadIcon("/snakeIcon256.png"));
         frame.setIconImages(icons);
 
         // Title, center window, exit kill all threads, and visible
@@ -41,4 +42,15 @@ public class Main {
     public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater(() -> { new Main(); }); // Lambda :O
     }
+
+    private Image loadIcon(String path) {
+        URL imgURL = getClass().getResource(path);
+        if (imgURL != null) {
+            return new ImageIcon(imgURL).getImage();
+        } else {
+            System.err.println("Couldn't find file: " + path);
+            return null;
+        }
+    }
+
 }
